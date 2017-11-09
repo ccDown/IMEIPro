489ff9e66a9873f782549297de83e82e60d07cec
kuan <1029671454@qq.com>
2017-11-09 10:41:16 +0800

    兼容6.0手机

--- app/src/main/java/soul/listener/com/imeipro/MainActivity.java
+++ app/src/main/java/soul/listener/com/imeipro/MainActivity.java
@@ -1,12 +1,17 @@
 package soul.listener.com.imeipro;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Method;
 
@@ -19,13 +24,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         final TextView tv = (TextView) findViewById(R.id.tv_imei);
         Button imei  = (Button) findViewById(R.id.btn_getimei);
 
         imei.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv.setText(getIMEI(MainActivity.this));
+                tv.setText(getIMEI(getApplicationContext()));
             }
         });
 
@@ -34,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         imei1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv.setText(getIMEI1(MainActivity.this));
+                tv.setText(getIMEI1(getApplicationContext()));
             }
         });
 
@@ -43,10 +49,17 @@ public class MainActivity extends AppCompatActivity {
         imei2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv.setText(getIMEI2(MainActivity.this));
+                tv.setText(getIMEI2(getApplicationContext()));
             }
         });
 
+        final Button btn_getPermission = (Button) findViewById(R.id.btn_getPermission);
+        btn_getPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getPermission();
+            }
+        });
     }
 
     /**获取MeID号*/
@@ -56,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
             teleMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
             return teleMgr.getDeviceId();
         } catch (Exception e) {
+            Log.e("MainActivity","Failed to get the hw info." + e);
             return UNKNOWN;
         }
     }
@@ -69,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             Method getImei=clazz.getDeclaredMethod("getImei",int.class);//(int slotId)
             return (String) getImei.invoke(teleMgr, 0);
         } catch (Exception e) {
+            Log.e("MainActivity","Failed to get the hw info." + e);
             return UNKNOWN;
         }
     }
@@ -82,7 +97,24 @@ public class MainActivity extends AppCompatActivity {
             Method getImei=clazz.getDeclaredMethod("getImei",int.class);//(int slotId)
             return (String) getImei.invoke(teleMgr, 1);
         } catch (Exception e) {
+            Log.e("MainActivity","Failed to get the hw info." + e);
             return UNKNOWN;
+        }
+    }
+
+    public void getPermission(){
+        //Android6.0需要动态获取权限
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
+        }
+    }
+    /**
+     *加个获取权限的监听
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,int[] grantResults) {
+        if (requestCode == 0 && grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(),"权限成功",Toast.LENGTH_SHORT).show();
         }
     }
 }
